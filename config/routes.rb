26943a107acc21d20a5fa7dff46bf7803fe04015
ddaniger <d.daniger6@gmail.com>
2022-04-04 19:15:11 +0000

    bug: fix admin scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
   devise_for :users, controllers: {
         sessions: 'users/sessions'
-      }
-  devise_for :admins, controllers: {
-        sessions: 'admins/sessions'
       }
   root "projects#index"
   resources :projects
