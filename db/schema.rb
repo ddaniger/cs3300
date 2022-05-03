792c79b12842a3b2e3f989b38013e976f73ac37e
ddaniger <d.daniger6@gmail.com>
2022-05-03 04:00:05 +0000

    feat: add css and html to do stuff

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_204207) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
+    t.text "abstract"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
